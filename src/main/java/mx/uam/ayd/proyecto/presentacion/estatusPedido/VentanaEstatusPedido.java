5fd326d0c689aff13649536589dabffe530fce50
Daniel <dano.prez.coa@gmail.com>
2021-12-12 22:20:49 -0600

    HU 3

---
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/estatusPedido/VentanaEstatusPedido.java
@@ -0,0 +1,189 @@
+package mx.uam.ayd.proyecto.presentacion.estatusPedido;
+
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.springframework.stereotype.Component;
+
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+
+
+@SuppressWarnings("serial")
+@Component
+public class VentanaEstatusPedido extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtPagoEnviado;
+	private JTextField txtDiseando;
+	private JTextField txtImprimiendo;
+	private JTextField txtEnviado;
+	private ControlEstatusPedido control;
+
+	public VentanaEstatusPedido() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(500, 300, 500, 280);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		// Nombre empresa
+		JLabel lblMiAplicacin = new JLabel("Diseños JEHC");
+		lblMiAplicacin.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblMiAplicacin.setBounds(146, 0, 141, 36);
+		contentPane.add(lblMiAplicacin);
+		
+		//Menu Navegacion
+		JMenuBar menuNav= new JMenuBar ();
+		menuNav.setBounds(10, 29, 464, 30);
+		contentPane.add(menuNav);
+		JMenu menu1,menu2,menu3,menu4,menu5,menu6;
+		menu1=new JMenu("Inicio");
+		menuNav.add(menu1);
+		menu2=new JMenu("Productos");
+		menuNav.add(menu2);
+		menu3=new JMenu("Diseños Previos");
+		menuNav.add(menu3);
+		menu4=new JMenu("¿Preguntas?");
+		menuNav.add(menu4);
+		menu5=new JMenu("Perfil");
+		menuNav.add(menu5);
+		menu6=new JMenu("Carrito");
+		menuNav.add(menu6);
+		
+		//titulo estatus del pedido
+		JLabel lblNewLabel = new JLabel("Estatus del pedido");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel.setBounds(10, 59, 190, 25);
+		contentPane.add(lblNewLabel);
+		
+		//Titulo folio
+		JLabel lblNewLabel_1 = new JLabel("Folio:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(10, 91, 33, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		//Comienzan los titulos de los estatus del pedido
+		txtPagoEnviado = new JTextField();
+		txtPagoEnviado.setEditable(false);
+		txtPagoEnviado.setText("| PAGO ENVIADO |");
+		txtPagoEnviado.setBounds(10, 116, 107, 20);
+		contentPane.add(txtPagoEnviado);
+		txtPagoEnviado.setColumns(10);
+
+		txtDiseando = new JTextField();
+		txtDiseando.setEditable(false);
+		txtDiseando.setText("| DISEÑANDO |");
+		txtDiseando.setColumns(10);
+		txtDiseando.setBounds(143, 116, 91, 20);
+		contentPane.add(txtDiseando);
+		
+		txtImprimiendo = new JTextField();
+		txtImprimiendo.setEditable(false);
+		txtImprimiendo.setText("| IMPRIMIENDO |");
+		txtImprimiendo.setColumns(10);
+		txtImprimiendo.setBounds(257, 116, 97, 20);
+		contentPane.add(txtImprimiendo);
+		
+		txtEnviado = new JTextField();
+		txtEnviado.setEditable(false);
+		txtEnviado.setText("| ENVIANDO |");
+		txtEnviado.setColumns(10);
+		txtEnviado.setBounds(377, 116, 78, 20);
+		contentPane.add(txtEnviado);
+		//Terminan los titulos del estatus del pedido
+
+		JLabel lblNewLabel_1_2 = new JLabel("Adjuntar comprobante de pago");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_2.setBounds(10, 196, 213, 25);
+		contentPane.add(lblNewLabel_1_2);
+		
+		JButton btnNewButton = new JButton("Enviar");
+		btnNewButton.setBounds(234, 199, 89, 23);
+		contentPane.add(btnNewButton);
+		
+	}
+
+	public void muestra (ControlEstatusPedido control, int i) {
+		//Obtenemos el FOLIO
+		JLabel lblNewLabel_1_1 = new JLabel(control.conseguirFolio(i));
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(50, 91, 78, 14);
+		contentPane.add(lblNewLabel_1_1);
+		
+		//Obtenemos el ESTADO DEL PAGO
+		boolean pago=control.EstatusPago(i);
+		//Checkbox PAGO ENVIADO
+		JCheckBox chckbxEnviandoP = new JCheckBox("En proceso");
+		chckbxEnviandoP.setEnabled(false);
+		chckbxEnviandoP.setSelected(!pago);
+		chckbxEnviandoP.setBounds(10, 143, 97, 23);
+		contentPane.add(chckbxEnviandoP);
+		
+		JCheckBox chckbxEnviandoR = new JCheckBox("Realizado");
+		chckbxEnviandoR.setEnabled(false);
+		chckbxEnviandoR.setSelected(pago);
+		chckbxEnviandoR.setBounds(10, 169, 97, 23);
+		contentPane.add(chckbxEnviandoR);
+		//Terminan checkbox PAGO ENVIADO
+
+		//Obtenemos el ESTADO DEL ESTADO DEL DISEÑO
+		boolean diseño=control.EstatusDisieño(i);
+		//Checkbox DISEÑANDO
+		JCheckBox chckbxDisP = new JCheckBox("En proceso");
+		chckbxDisP.setEnabled(false);
+		chckbxDisP.setSelected(!diseño);
+		chckbxDisP.setBounds(137, 143, 97, 23);
+		contentPane.add(chckbxDisP);
+
+		JCheckBox chckbxDisR = new JCheckBox("Realizado");
+		chckbxDisR.setEnabled(false);
+		chckbxDisR.setSelected(diseño);
+		chckbxDisR.setBounds(137, 169, 97, 23);
+		contentPane.add(chckbxDisR);
+		//Terminan checkbox DISEÑANDO
+
+		//Obtenemos el ESTADO DE LA IMPRESION
+		boolean impresion=control.EstatusImprimiendo(i);
+		//Checkbox IMPRIMIENDO
+		JCheckBox chckbxImpP = new JCheckBox("En proceso");
+		chckbxImpP.setSelected(!impresion);
+		chckbxImpP.setEnabled(false);
+		chckbxImpP.setBounds(257, 143, 97, 23);
+		contentPane.add(chckbxImpP);
+		
+		JCheckBox chckbxImpR = new JCheckBox("Realizado");
+		chckbxImpR.setEnabled(false);
+		chckbxImpR.setSelected(impresion);
+		chckbxImpR.setBounds(257, 169, 97, 23);
+		contentPane.add(chckbxImpR);
+		//Terminan checkBox IMPRIMIENDO
+
+		//Obtenemos el ESTADO DE EL ENVIO
+		boolean envio=control.EstatusEnvio(i);
+		//Checkbox ENVIANDO
+		JCheckBox chckbxEnvP = new JCheckBox("En proceso");
+		chckbxEnvP.setSelected(!envio);
+		chckbxEnvP.setEnabled(false);
+		chckbxEnvP.setBounds(377, 143, 97, 23);
+		contentPane.add(chckbxEnvP);
+		
+		JCheckBox chckbxEnvR = new JCheckBox("Realizado");
+		chckbxEnvR.setEnabled(false);
+		chckbxEnvR.setSelected(envio);
+		chckbxEnvR.setBounds(377, 169, 97, 23);
+		contentPane.add(chckbxEnvR);
+		//Terminan checkbox ENVIANDO
+		this.control=control;
+		setVisible(true);
+		}
+}
---
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/perfil/VentanaPerfil.java
@@ -0,0 +1,152 @@
+package mx.uam.ayd.proyecto.presentacion.perfil;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.springframework.stereotype.Component;
+
+import lombok.var;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JTextArea;
+import javax.swing.JScrollBar;
+import javax.swing.SwingConstants;
+
+@SuppressWarnings("serial")
+@Component
+public class VentanaPerfil extends JFrame {
+
+	private JPanel contentPane;
+	private ControlPerfil control;
+	public int btnIncremento=1;
+	public VentanaPerfil() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(500, 300, 500, 280);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		// Nombre empresa
+		JLabel lblMiAplicacin = new JLabel("Diseños JEHC");
+		lblMiAplicacin.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblMiAplicacin.setBounds(146, 0, 141, 36);
+		contentPane.add(lblMiAplicacin);
+		
+		//Menu Navegacion
+		JMenuBar menuNav= new JMenuBar ();
+		menuNav.setBounds(10, 34, 500, 30);
+		contentPane.add(menuNav);
+		JMenu menu1,menu2,menu3,menu4,menu5,menu6;
+		menu1=new JMenu("Inicio");
+		menuNav.add(menu1);
+		menu2=new JMenu("Productos");
+		menuNav.add(menu2);
+		menu3=new JMenu("Diseños Previos");
+		menuNav.add(menu3);
+		menu4=new JMenu("¿Preguntas?");
+		menuNav.add(menu4);
+		menu5=new JMenu("Perfil");
+		menuNav.add(menu5);
+		menu6=new JMenu("Carrito");
+		menuNav.add(menu6);
+		
+		//titulo perfil
+		JLabel lblNewLabel = new JLabel("Perfil");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel.setBounds(10, 69, 54, 25);
+		contentPane.add(lblNewLabel);
+		
+		//Titulo de bienvenida
+		JLabel lblNewLabel_1 = new JLabel("¡Hola");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(74, 76, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		//Titulo pedidos
+		JLabel lblNewLabel_1_1 = new JLabel("Pedidos");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(10, 105, 54, 14);
+		contentPane.add(lblNewLabel_1_1);
+
+		//Scrollbar
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setToolTipText("");
+		scrollBar.setBounds(10, 131, 11, 77);
+		contentPane.add(scrollBar);
+		
+		//Titulo informacion
+		JLabel lblNewLabel_1_1_1 = new JLabel("Información");
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_1_1.setBounds(235, 105, 115, 14);
+		contentPane.add(lblNewLabel_1_1_1);
+		
+		//Botones de informacion
+		JButton btnNewButton = new JButton("Informacion Personal");
+		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton.setBounds(235, 131, 174, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnInformacionDeLa = new JButton("Informacion de la cuenta");
+		btnInformacionDeLa.setHorizontalAlignment(SwingConstants.LEFT);
+		btnInformacionDeLa.setBounds(235, 170, 174, 23);
+		contentPane.add(btnInformacionDeLa);
+		//Terminan botones de informacion
+	}
+
+	public void muestra (ControlPerfil control) {
+	this.control=control;
+	//Recupera el nombre del cliente y lo escribe
+	JLabel lblNewLabel_2 = new JLabel(control.conseguirNombre());
+	lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	lblNewLabel_2.setBounds(110, 76, 70, 14);
+	
+	//Recupera el numero de pedidos existentes
+	int existentes=control.pedidosExistentes();
+	int aumento1=129, aumento2=130;
+	JButton[] btnDetalles = new JButton[existentes];
+	//CREAMOS LOS BOTONES DE DETALLES Y LOS FOLIOS DE LOS PEDIDOS
+	for (int i=0;i<existentes;i++){
+		//CREAMOS BOTONES DE DETALLES
+		btnDetalles[i]=new JButton("Detalles");
+		btnDetalles[i].addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			control.estatusPedido(1);
+		}
+	});
+	btnDetalles[i].setHorizontalAlignment(SwingConstants.LEADING);
+	btnDetalles[i].setBounds(140, aumento1, 85, 25);
+	contentPane.add(btnDetalles[i]);
+	//Folios de los pedidos
+	//Recupera el numero de FOLIO
+	JTextArea txtPedido1_1 = new JTextArea(control.recuperaFolio(i+1));
+	txtPedido1_1.setEditable(false);
+	txtPedido1_1.setBounds(58, aumento2, 82, 25);
+	contentPane.add(txtPedido1_1);
+
+	//Comienzan a mostrarse los pedidos
+	JTextArea txtFolio1 = new JTextArea("Folio");
+	txtFolio1.setEditable(false);
+	txtFolio1.setBounds(20, aumento2, 35, 25);
+	contentPane.add(txtFolio1);
+
+	//Incrementamos las variables
+	aumento1=aumento1+27;
+	aumento2=aumento2+26;
+	btnIncremento++;
+	}
+
+	//Terminan de mostrarse los pedidos 
+	contentPane.add(lblNewLabel_2);
+	setVisible(true);
+	}
+}
